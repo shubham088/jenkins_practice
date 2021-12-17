2ff67a2195a50007362567edfbb4748316628947
shubham088 <shubhamsharma0296@gmail.com>
2021-12-17 16:08:49 +0530

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,24 +1,29 @@
 import unittest
-from code import *
+from code import Func
 
 
 class Testing(unittest.TestCase):
     def test_sum_0(self):
         a = 'some'
         b = 'some'
-        self.assertEqual(sum(5, 5),  10)
+        f = Func()
+        self.assertEqual(f.sum(5, 5),  10)
 
     def test_sum_1(self):
         a = True
         b = True
-        self.assertEqual(sum(10), 20)
+        f = Func()
+        self.assertEqual(f.sum(10), 20)
         
-    #def test_diff_1(self):
-    #    self.assertEqual(diff(5), 6)
+    def test_diff_1(self):
+        f = Func()
+        self.assertEqual(f.diff(5), 5)
         
-        
-    #def test_diff_2(self):
-    #    self.assertEqual(diff(6,5), 1)
+     
+    def test_diff_2(self):
+        f = Func()
+        self.assertEqual(f.diff(6,5), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
