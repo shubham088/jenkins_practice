6348999a64142b155cba70f7a6936353f048996a
shubham088 <shubhamsharma0296@gmail.com>
2021-12-17 11:17:55 +0530

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,24 @@
+import unittest
+from code import *
+
+
+class Testing(unittest.TestCase):
+    def test_sum_0(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(sum(5, 5),  10)
+
+    def test_sum_1(self):
+        a = True
+        b = True
+        self.assertEqual(sum(10), 20)
+        
+    def test_diff_1(self):
+        self.assertEqual(diff(5), 6)
+        
+        
+    def test_diff_2(self):
+        self.assertEqual(diff(6,5), 1)
+
+if __name__ == '__main__':
+    unittest.main()
