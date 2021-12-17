f6f57487f262be55d81136a4009b87cc10777d5a
shubham088 <shubhamsharma0296@gmail.com>
2021-12-17 16:07:53 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,28 +1,18 @@
-def sum(*a):
-    sum = 0
-    if len(a) == 1:
-        return a[0]+10
-    for x in a:
-        sum+=x
-    return sum 
-
-
-
-def diff(*a):
-    diff = 0
-    if len(a) == 1:
-        return a[0]
-    elif len(a) == 2:
-        return a[0] - a[1]
-    else:
-        return "too much data"
-
-print("sum = ", sum(5,5))
-
-print("sum = ", sum(10))
-
-print("diff = ", diff(5))
-
-print("diff = ", diff(6,3))
-
-print("diff  = ", diff(5,6,7))
+class Func:
+	def sum(self, *a):
+	    sum = 0
+	    if len(a) == 1:
+	        return a[0]+10
+	    for x in a:
+	        sum+=x
+	    return sum
+	    
+	def diff(self,*a):
+	    diff = 0
+	    if len(a) == 1:
+	        return a[0]
+	    elif len(a) == 2:
+	        return a[0] - a[1]
+	    else:
+	        return "too much data"
+     
