ed54f7da4eea1f217215fc1235839fd56a728d8a
shubham088 <shubhamsharma0296@gmail.com>
2021-12-27 13:23:26 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -13,3 +13,29 @@ def qualityGateAnalysis(int timer, String credential_id, String data){
    sleep timer
    waitForQualityGate abortPipeline: true, credentialsId: credential_id
 }
+
+def artifactoryUpload(int build, String serverID, String artifactoryFolderName, String pat, String tar){
+  String[] pattern = pat.split(" ")
+  String[] target  = tar.split(" ")
+  
+  rtUpload (
+  serverId: serverID,
+  spec: '''{
+        "files": [
+          {
+            "pattern": pattern[0],
+            "target":  artifactoryFolderName+target[0]+build.toString()
+          },
+          {
+            "pattern": pattern[1],
+            "target" : artifactoryFolderName+target[1]+build.toString(),
+            "recursive":true
+          }
+       ]
+      }''',
+
+  )
+  
+ echo "done uploading artifacts using shared lib"
+
+}
