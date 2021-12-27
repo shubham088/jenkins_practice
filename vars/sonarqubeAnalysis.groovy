18b03682113a0ae8239e45f6cf1329c9bf43cb87
shubham088 <shubhamsharma0296@gmail.com>
2021-12-27 12:14:16 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -7,7 +7,8 @@ def sonarAnalysis(String sonarServerName, String sonarScanner) {
 
 def qualityGateAnalysis(int timer, String credential_id, String data){
   echo "$data"
-  
+  my_data = data.split(" ")
+  echo "$my_data[0]"
    sleep timer
    waitForQualityGate abortPipeline: true, credentialsId: credential_id
 }
