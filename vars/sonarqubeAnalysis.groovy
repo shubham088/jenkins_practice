2adc85fa5e3dc81f7cf6077ad8cc909099fc613f
shubham088 <shubhamsharma0296@gmail.com>
2021-12-27 10:39:22 +0530

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,8 +1,6 @@
 def sonarAnalysis(String sonarServerName, String sonarScanner) {
 //   echo "${sonarServerName}"
 //   echo "${sonarScanner}"
-  println(this.env.BUILD)
-  println(env.BUILD)
   withSonarQubeEnv(sonarServerName) {
     sh sonarScanner+"/bin/sonar-scanner"
   }
